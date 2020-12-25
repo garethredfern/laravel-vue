1b652b1e1e8b0ea741632e3f22affe5cdb251c52
Gareth Redfern <garethr@hey.com>
2020-12-25 22:23:47 +0000

    add auth vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import * as auth from "@/store/modules/auth";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  getters: {},
+  strict: true,
+
+  modules: {
+    auth,
+  },
 });
---
+++ src/store/modules/auth.js
@@ -0,0 +1,78 @@
+import AuthService from "@/services/AuthService";
+
+export const namespaced = true;
+
+export const state = {
+  user: null,
+  loading: false,
+  error: null,
+};
+
+export const mutations = {
+  SET_USER(state, user) {
+    state.user = user;
+  },
+  CLEAR_USER() {
+    window.localStorage.clear();
+    location.reload();
+  },
+  SET_LOADING(state, loading) {
+    state.loading = loading;
+  },
+  SET_MESSAGE(state, message) {
+    state.message = message;
+  },
+  SET_ERROR(state, error) {
+    state.error = error;
+  },
+};
+
+export const actions = {
+  login({ commit, dispatch }, payload) {
+    commit("SET_LOADING", true);
+    return AuthService.login(payload)
+      .then(() => {
+        dispatch("getAuthUser");
+      })
+      .catch((error) => {
+        commit("SET_LOADING", false);
+        commit("SET_ERROR", error.data ? error.data.message : error);
+      });
+  },
+  logout({ commit }) {
+    return AuthService.logout()
+      .then(() => {
+        commit("CLEAR_USER");
+      })
+      .catch(() => {
+        commit("CLEAR_USER");
+      });
+  },
+  getAuthUser({ commit }) {
+    commit("SET_LOADING", true);
+    return AuthService.getAuthUser()
+      .then((response) => {
+        commit("SET_USER", response.data);
+        commit("SET_LOADING", false);
+      })
+      .catch((error) => {
+        commit("SET_LOADING", false);
+        commit("SET_ERROR", error.data ? error.data.message : error);
+      });
+  },
+};
+
+export const getters = {
+  authUser: (state) => {
+    return state.user;
+  },
+  error: (state) => {
+    return state.error;
+  },
+  loading: (state) => {
+    return state.loading;
+  },
+  loggedIn: (state) => {
+    return !!state.user;
+  },
+};
