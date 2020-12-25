596c195496f978784616ebf7121957e4cf2894e2
Gareth Redfern <garethr@hey.com>
2020-12-25 22:26:08 +0000

    create login and dashboard template views

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2 class="font-bold text-xl">Dashboard</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2 class="font-bold text-xl">Login Page</h2>
+    <div class="grid grid-cols-2">
+      <button type="button" @click="login" class="w-full">Login</button>
+      <button type="button" @click="forgotPassword" class="w-full">
+        Forgot Password
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthService from "@/services/AuthService";
+
+export default {
+  name: "Login",
+  methods: {
+    login() {
+      this.$store.dispatch("auth/login");
+    },
+    async forgotPassword() {
+      try {
+        await AuthService.forgotPassword();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
