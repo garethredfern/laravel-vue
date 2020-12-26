e35973cc52b2ceb2ea3d41421ded26f202e9150f
Gareth Redfern <garethr@hey.com>
2020-12-26 21:32:56 +0000

    add a base button component

---
+++ src/components/BaseBtn.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    :type="type"
+    v-on="$listeners"
+    class="px-2.5 py-1.5 border border-transparent text-xs rounded shadow-sm text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 text-center"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseBtn",
+  props: {
+    type: {
+      type: String,
+      default: "submit",
+    },
+    text: {
+      type: String,
+      default: "Submit",
+    },
+  },
+};
+</script>
