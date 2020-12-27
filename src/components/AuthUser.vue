1a61838b22052c831b586e6385ed0f78d5ef7372
Gareth Redfern <gareth@garethredfern.com>
2020-12-27 11:03:18 +0000

    add AuthUser component

---
+++ src/components/AuthUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="max-w-sm p-5">
+    <h2 class="font-bold text-xl text-center mb-4">User Details</h2>
+    <ul class="border p-5 rounded">
+      <li>Name: {{ authUser.name }}</li>
+      <li>Email: {{ authUser.email }}</li>
+      <li v-if="authUser.email_verified_at">Emailed Verified</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "AuthUser",
+  computed: {
+    ...mapGetters("auth", ["authUser"]),
+  },
+};
+</script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,7 @@
   <div class="p-5">
     <h2 class="font-bold text-xl">Dashboard</h2>
     <div class="flex space-x-2">
+      <AuthUser />
       <UpdatePassword />
       <VerifyEmail />
     </div>
@@ -9,12 +10,14 @@
 </template>
 
 <script>
+import AuthUser from "@/components/AuthUser";
 import VerifyEmail from "@/components/VerifyEmail";
 import UpdatePassword from "@/components/UpdatePassword";
 
 export default {
   name: "Dashboard",
   components: {
+    AuthUser,
     VerifyEmail,
     UpdatePassword,
   },
