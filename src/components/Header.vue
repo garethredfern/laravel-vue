139f1478894722c3cef0f97f2e5156e4d1b5241d
Gareth Redfern <gareth@garethredfern.com>
2021-01-03 22:28:56 +0000

    add conditional for home & dashboard links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="p-5 text-white bg-blue-400">
     <nav class="container flex items-center justify-between mx-auto">
-      <router-link to="/">Home</router-link>
+      <router-link to="/dashboard" v-if="authUser">Dashboard</router-link>
+      <router-link to="/" v-else>Home</router-link>
       <div class="flex space-x-4" v-if="authUser">
-        <router-link to="/dashboard">Dashboard</router-link>
         <Logout />
       </div>
       <router-link to="/login" v-else>Login</router-link>
