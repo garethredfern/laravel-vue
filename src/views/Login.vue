398e1850ec074f9962449627ede5c14977840c13
Gareth Redfern <gareth@garethredfern.com>
2021-01-04 14:48:57 +0000

    add register link to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,10 @@
   <div class="max-w-sm p-5 m-auto">
     <h2 class="mb-4 text-xl font-bold text-center">Login</h2>
     <LoginForm />
+    <p class="text-center text-gray-500">
+      <router-link to="/register" class="text-blue-500">Register</router-link>
+      for an account.
+    </p>
   </div>
 </template>
 
