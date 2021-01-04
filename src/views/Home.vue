d81043e8843aba67123943933a4cddd0f9e379b1
Gareth Redfern <gareth@garethredfern.com>
2021-01-04 14:56:49 +0000

    add home page text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="max-w-sm p-5 m-auto"></div>
+  <div class="max-w-sm p-5 m-auto">
+    <article class="text-lg text-center">
+      <p>Welcome to the Laravel Vue Demo app.</p>
+      <p>
+        You can register for an account
+        <router-link to="/register" class="text-blue-500">here</router-link>.
+      </p>
+    </article>
+  </div>
 </template>
 
 <script>
