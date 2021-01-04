af3016084ce64987a5ed0e749e66afbf89cb6d7e
Gareth Redfern <gareth@garethredfern.com>
2021-01-04 15:14:53 +0000

    add user view and remove user details from dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,21 +1,11 @@
 <template>
-  <div class="p-5">
-    <div class="flex space-x-2">
-      <AuthUser />
-      <UpdatePassword />
-    </div>
+  <div class="py-5">
+    <p>Welcome to your Dashboard</p>
   </div>
 </template>
 
 <script>
-import AuthUser from "@/components/AuthUser";
-import UpdatePassword from "@/components/UpdatePassword";
-
 export default {
   name: "Dashboard",
-  components: {
-    AuthUser,
-    UpdatePassword,
-  },
 };
 </script>
---
+++ src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="py-5">
+    <div class="flex space-x-10">
+      <AuthUser />
+      <UpdatePassword />
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthUser from "@/components/AuthUser";
+import UpdatePassword from "@/components/UpdatePassword";
+
+export default {
+  name: "Dashboard",
+  components: {
+    AuthUser,
+    UpdatePassword,
+  },
+};
+</script>
