ccd21d4ed5b314bfbaaff10200dcc046c99ee0bf
Gareth Redfern <gareth@garethredfern.com>
2021-01-05 07:51:01 +0000

    move flas message into form - update password

--- src/components/UpdatePassword.vue
+++ src/components/UpdatePassword.vue
@@ -1,45 +1,42 @@
 <template>
-  <div class="max-w-sm">
-    <h2 class="mb-2 text-lg font-bold text-center">Update Password</h2>
-    <form
-      @submit.prevent="updatePassword"
-      class="p-5 bg-white border rounded shadow"
-    >
-      <div class="mb-2">
-        <label for="password" class="text-sm text-gray-500"
-          >Current Password</label
-        >
-        <input
-          type="password"
-          id="current-password"
-          v-model="currentPassword"
-          class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
-      <div class="mb-2">
-        <label for="password" class="text-sm text-gray-500">Password</label>
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
-      <div class="mb-4">
-        <label for="password-confirm" class="text-sm text-gray-500"
-          >Confirm Password</label
-        >
-        <input
-          type="password"
-          id="password-confirm"
-          v-model="passwordConfirm"
-          class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
-      <BaseBtn type="submit" text="Update Password" />
-    </form>
+  <form
+    @submit.prevent="updatePassword"
+    class="p-5 bg-white border rounded shadow"
+  >
+    <div class="mb-2">
+      <label for="password" class="text-sm text-gray-500"
+        >Current Password</label
+      >
+      <input
+        type="password"
+        id="current-password"
+        v-model="currentPassword"
+        class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+      />
+    </div>
+    <div class="mb-2">
+      <label for="password" class="text-sm text-gray-500">Password</label>
+      <input
+        type="password"
+        id="password"
+        v-model="password"
+        class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+      />
+    </div>
+    <div class="mb-4">
+      <label for="password-confirm" class="text-sm text-gray-500"
+        >Confirm Password</label
+      >
+      <input
+        type="password"
+        id="password-confirm"
+        v-model="passwordConfirm"
+        class="block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+      />
+    </div>
+    <BaseBtn type="submit" text="Update" />
     <FlashMessage :message="message" :error="error" />
-  </div>
+  </form>
 </template>
 
 <script>
