52de1f2859ed7fb9e8eed556997378b3ce3237c3
Gareth Redfern <gareth@garethredfern.com>
2021-01-10 10:49:51 +0000

    add home page info boxes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="max-w-sm p-5 m-auto">
-    <article class="text-lg text-center">
+  <div class="max-w-lg p-5 m-auto">
+    <article class="p-5 text-lg text-center bg-white border rouned">
       <p>Welcome to the Laravel Vue Demo app.</p>
       <p>
         You can register for an account
         <router-link to="/register" class="text-blue-500">here</router-link>.
       </p>
+    </article>
+    <article class="p-5 mt-5 text-lg text-center bg-white border rouned">
+      Docs on how this application is built can be found at:
+      <a href="https://laravelvuespa.com/" class="base-link"
+        >laravelvuespa.com</a
+      >
     </article>
   </div>
 </template>
