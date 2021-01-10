c265aa636c034e34400b77befbdf2ab5ae13f2d1
Gareth Redfern <gareth@garethredfern.com>
2021-01-10 10:25:09 +0000

    add view profile link to dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="py-5">
-    <p>Welcome to your Dashboard</p>
+    <p class="mb-4 text-gray-400">
+      Full documentation for how this application is built can be found at
+      <a href="https://laravelvuespa.com/" class="base-link"
+        >laravelvuespa.com</a
+      >.
+    </p>
+    <div class="grid gap-10 md:grid-cols-3">
+      <ul class="p-5 bg-white border rounded shadow">
+        <li>
+          <router-link to="/user" class="base-link"> View Profile </router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
