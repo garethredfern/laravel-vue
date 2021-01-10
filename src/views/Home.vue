096dc7ecb23b60ed6d71391c864416b6d457bcf1
Gareth Redfern <gareth@garethredfern.com>
2021-01-10 11:09:39 +0000

    fix typo and add p tag to home text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,13 @@
         <router-link to="/register" class="text-blue-500">here</router-link>.
       </p>
     </article>
-    <article class="p-5 mt-5 text-lg text-center bg-white border rouned">
-      Docs on how this application is built can be found at:
-      <a href="https://laravelvuespa.com/" class="base-link"
-        >laravelvuespa.com</a
-      >
+    <article class="p-5 mt-5 text-lg text-center bg-white border rounded">
+      <p>
+        Docs on how this application is built can be found at:
+        <a href="https://laravelvuespa.com/" class="base-link"
+          >laravelvuespa.com</a
+        >
+      </p>
     </article>
   </div>
 </template>
