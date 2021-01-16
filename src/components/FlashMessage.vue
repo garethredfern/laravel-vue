6f488391ad761a327e0a770330030633cbdff3a7
Gareth Redfern <gareth@garethredfern.com>
2021-01-16 10:27:21 +0000

    transition the flash message with a fade effect

--- src/components/FlashMessage.vue
+++ src/components/FlashMessage.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <p v-if="message" class="mt-2 text-sm text-green-500">
-      {{ message }}
-    </p>
-    <p v-if="error" class="mt-2 text-sm text-red-500">
-      {{ error }}
-    </p>
+    <transition-group name="fade">
+      <p v-if="message" class="mt-2 text-sm text-green-500" key="message">
+        {{ message }}
+      </p>
+      <p v-if="error" class="mt-2 text-sm text-red-500" key="error">
+        {{ error }}
+      </p>
+    </transition-group>
   </div>
 </template>
 
