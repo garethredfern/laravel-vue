12c0c328186bd69bcd597c29f81583bb57a4b2b4
Gareth Redfern <gareth@garethredfern.com>
2021-01-25 23:01:52 +0000

    refactor getError method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,11 @@
 export const getError = (error) => {
+  const errorMessage = "API Error, please try again.";
+
   if (!error.response) {
     console.error(`API ${error.config.url} not found`);
+    return errorMessage;
   }
-  if (error.response && process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === "development") {
     console.error(error.response.data);
     console.error(error.response.status);
     console.error(error.response.headers);
@@ -10,5 +13,6 @@ export const getError = (error) => {
   if (error.response.data && error.response.data.errors) {
     return error.response.data.errors;
   }
-  return "API Error, please try again.";
+
+  return errorMessage;
 };
