f3f10fbe0219e5045cabdd59b3c5ef69b8fceab2
Gareth Redfern <gareth@garethredfern.com>
2021-01-26 06:29:26 +0000

    add grid col 1 for small devices

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-5 xl:px-0">
-    <div class="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+    <div class="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
       <AuthUser class="p-5 bg-white border rounded shadow" />
       <AuthUserForm class="p-5 bg-white border rounded shadow" />
       <UpdatePassword class="p-5 bg-white border rounded shadow" />
