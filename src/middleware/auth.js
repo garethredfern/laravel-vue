c2b73c2e9a5556a4d835ac0d4cc2675433e02ab6
Gareth Redfern <gareth@garethredfern.com>
2021-01-29 23:09:54 +0000

    refactor auth middleware, remove authUser const

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,8 +1,7 @@
 export default function auth({ to, next, store }) {
-  const authUser = store.getters["auth/authUser"];
   const loginQuery = { path: "/login", query: { redirect: to.fullPath } };
 
-  if (!authUser) {
+  if (!store.getters["auth/authUser"]) {
     store.dispatch("auth/getAuthUser").then(() => {
       if (!store.getters["auth/authUser"]) next(loginQuery);
       else next();
