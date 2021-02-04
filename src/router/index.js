7f9fec646985dfa29700e24edce8d3b463fab8fb
Mark Nuyens <mark@emozio.nl>
2021-02-04 19:14:48 +0100

    Added scrollbehavior function for VueRouter instance
    This will ensure page scrolling position between page history navigation.

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,13 @@ const routes = [
 const router = new VueRouter({
   mode: "history",
   routes,
+  scrollBehavior(to, from, savedPosition) {
+	if (savedPosition) {
+		return savedPosition
+	} else {
+		return { x: 0, y: 0 }
+	}
+  }
 });
 
 router.beforeEach((to, from, next) => {
