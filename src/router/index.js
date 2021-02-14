83afe182e449abb8cc3ca7676d124ce0cff418ac
Gareth Redfern <gareth@garethredfern.com>
2021-02-14 10:22:54 +0000

    fix code indent

--- src/router/index.js
+++ src/router/index.js
@@ -75,12 +75,12 @@ const router = new VueRouter({
   mode: "history",
   routes,
   scrollBehavior(to, from, savedPosition) {
-	if (savedPosition) {
-		return savedPosition
-	} else {
-		return { x: 0, y: 0 }
-	}
-  }
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
 });
 
 router.beforeEach((to, from, next) => {
