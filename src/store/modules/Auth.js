0fcd78fe84962214a0efb0711df2ea8275064e05
Gareth Redfern <gareth@garethredfern.com>
2021-02-14 10:17:00 +0000

    refactor getAuthUser to return a user on success

--- src/store/modules/Auth.js
+++ src/store/modules/Auth.js
@@ -35,18 +35,18 @@ export const actions = {
         commit("SET_ERROR", getError(error));
       });
   },
-  getAuthUser({ commit }) {
+  async getAuthUser({ commit }) {
     commit("SET_LOADING", true);
-    return AuthService.getAuthUser()
-      .then((response) => {
-        commit("SET_USER", response.data.data);
-        commit("SET_LOADING", false);
-      })
-      .catch((error) => {
-        commit("SET_LOADING", false);
-        commit("SET_USER", null);
-        commit("SET_ERROR", getError(error));
-      });
+    try {
+      const response = await AuthService.getAuthUser();
+      commit("SET_USER", response.data.data);
+      commit("SET_LOADING", false);
+      return response.data.data;
+    } catch (error) {
+      commit("SET_LOADING", false);
+      commit("SET_USER", null);
+      commit("SET_ERROR", getError(error));
+    }
   },
   setGuest(context, { value }) {
     window.localStorage.setItem("guest", value);
