40dfa907649b800d596af006e7fa0bbdf2218c76
Gareth Redfern <gareth@garethredfern.com>
2021-02-15 22:22:47 +0000

    refactor auth to return axios response

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -29,28 +29,28 @@ authClient.interceptors.response.use(
 export default {
   async login(payload) {
     await authClient.get("/sanctum/csrf-cookie");
-    await authClient.post("/login", payload);
+    return authClient.post("/login", payload);
   },
   logout() {
     return authClient.post("/logout");
   },
   async forgotPassword(payload) {
     await authClient.get("/sanctum/csrf-cookie");
-    await authClient.post("/forgot-password", payload);
+    return authClient.post("/forgot-password", payload);
   },
   getAuthUser() {
     return authClient.get("/api/users/auth");
   },
   async resetPassword(payload) {
     await authClient.get("/sanctum/csrf-cookie");
-    await authClient.post("/reset-password", payload);
+    return authClient.post("/reset-password", payload);
   },
   updatePassword(payload) {
     return authClient.put("/user/password", payload);
   },
   async registerUser(payload) {
     await authClient.get("/sanctum/csrf-cookie");
-    await authClient.post("/register", payload);
+    return authClient.post("/register", payload);
   },
   sendVerification(payload) {
     return authClient.post("/email/verification-notification", payload);
