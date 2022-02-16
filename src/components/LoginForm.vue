a8fa528ba7df111b4451c84fd83397a5901bd03c
Gareth Redfern <gareth@garethredfern.com>
2022-02-16 21:52:30 +0000

    add login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from "vue";
+import { useAuth } from "@/stores/authStore";
+import BaseBtn from "@/components/BaseBtn.vue";
+import BaseInput from "@/components/BaseInput.vue";
+import FlashMessage from "@/components/FlashMessage.vue";
+
+const authStore = useAuth();
+const email = ref(null);
+const password = ref(null);
+</script>
+
+<template>
+  <form @submit.prevent="authStore.login({ email, password })">
+    <BaseInput
+      type="email"
+      label="Email"
+      name="email"
+      v-model="email"
+      autocomplete="email"
+      placeholder="luke@jedi.com"
+      required
+      class="mb-4"
+    />
+    <BaseInput
+      type="password"
+      label="Password"
+      name="password"
+      v-model="password"
+      class="mb-4"
+      autocomplete
+    />
+    <div class="flex justify-between items-center">
+      <BaseBtn type="submit" text="Login" />
+      <router-link
+        to="/forgot-password"
+        class="base-link text-sm text-gray-400"
+      >
+        Forgot your password?
+      </router-link>
+    </div>
+    <FlashMessage :error="authStore.authError" />
+  </form>
+</template>
