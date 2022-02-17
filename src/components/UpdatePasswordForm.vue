27439fb74cd77fc2794315579b4a693964e718f2
Gareth Redfern <gareth@garethredfern.com>
2022-02-17 10:22:57 +0000

    add update password form

---
+++ src/components/UpdatePasswordForm.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref } from "vue";
+import { getError } from "@/utils/helpers";
+import BaseBtn from "@/components/BaseBtn.vue";
+import AuthService from "@/services/AuthService";
+import BaseInput from "@/components/BaseInput.vue";
+import FlashMessage from "@/components/FlashMessage.vue";
+
+const currentPassword = ref(null);
+const password = ref(null);
+const passwordConfirm = ref(null);
+const authError = ref(null);
+const message = ref(null);
+
+async function updatePassword() {
+  authError.value = null;
+  message.value = null;
+  const payload = {
+    current_password: currentPassword,
+    password: password,
+    password_confirmation: passwordConfirm,
+  };
+  try {
+    const response = await AuthService.updatePassword(payload);
+    message.value = response.data.status;
+  } catch (error) {
+    authError.value = getError(error);
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="updatePassword">
+    <BaseInput
+      type="password"
+      label="Current Password"
+      name="current-password"
+      v-model="currentPassword"
+      required
+      class="mb-2"
+    />
+    <BaseInput
+      type="password"
+      label="Password"
+      name="password"
+      v-model="password"
+      required
+      class="mb-2"
+    />
+    <BaseInput
+      type="password"
+      label="Confirm Password"
+      name="password-confirm"
+      v-model="passwordConfirm"
+      class="mb-4"
+    />
+    <BaseBtn type="submit" text="Update" />
+    <FlashMessage :message="message" :error="authError" />
+  </form>
+</template>
