2dd3d7c9151fd3a620caec0a0f98b8e0d35e4805
Gareth Redfern <gareth@garethredfern.com>
2022-02-18 10:48:15 +0000

    add update password form to user page

---
+++ src/views/User.vue
@@ -0,0 +1,12 @@
+<script setup>
+import UpdatePasswordForm from "@/components/UpdatePasswordForm.vue";
+</script>
+
+<template>
+  <div class="p-5">
+    <h2 class="mb-4 text-xl font-bold">User Details</h2>
+    <div class="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
+      <UpdatePasswordForm class="p-5 bg-white border rounded shadow" />
+    </div>
+  </div>
+</template>
