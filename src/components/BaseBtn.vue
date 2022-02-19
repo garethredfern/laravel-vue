16e03fb35c8c54ac0ba61f4dfe7ded485c2a6d39
Gareth Redfern <gareth@garethredfern.com>
2022-02-19 13:48:42 +0000

    refactor base button and base input to use the composition API

--- src/components/BaseBtn.vue
+++ src/components/BaseBtn.vue
@@ -1,24 +1,21 @@
-<script>
-export default {
-  name: "BaseBtn",
-  props: {
-    type: {
-      type: String,
-      default: "submit",
-    },
-    text: {
-      type: String,
-      default: "Submit",
-    },
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: "submit",
   },
-};
+  text: {
+    type: String,
+    default: "Submit",
+  },
+});
 </script>
 
 <template>
   <button
-    :type="type"
+    :type="props.type"
     class="rounded border border-transparent bg-blue-400 px-2.5 py-1.5 text-center text-xs text-white shadow-sm transition hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
   >
-    {{ text }}
+    {{ props.text }}
   </button>
 </template>
--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,55 +1,52 @@
-<script>
-export default {
-  name: "BaseInput",
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    label: {
-      type: String,
-      default: "",
-    },
-    type: {
-      type: String,
-      default: "text",
-    },
-    showLabel: {
-      type: Boolean,
-      default: true,
-    },
-    required: {
-      type: Boolean,
-      default: false,
-    },
-    placeholder: {
-      type: String,
-      default: null,
-    },
-    autocomplete: {
-      type: String,
-      default: null,
-    },
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
   },
-};
+  label: {
+    type: String,
+    default: null,
+  },
+  type: {
+    type: String,
+    default: "text",
+  },
+  showLabel: {
+    type: Boolean,
+    default: true,
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  placeholder: {
+    type: String,
+    default: null,
+  },
+  autocomplete: {
+    type: String,
+    default: null,
+  },
+});
 </script>
 
 <template>
   <div>
     <label
-      :for="name"
+      :for="props.name"
       class="text-sm text-gray-500"
-      :class="{ 'sr-only': !showLabel }"
-      v-if="label"
+      :class="{ 'sr-only': !props.showLabel }"
+      v-if="props.label"
     >
-      {{ label }}
+      {{ props.label }}
     </label>
     <input
-      :id="name"
-      :type="type"
-      :required="required"
-      :placeholder="placeholder"
-      :autocomplete="autocomplete"
+      :id="props.name"
+      :type="props.type"
+      :required="props.required"
+      :placeholder="props.placeholder"
+      :autocomplete="props.autocomplete"
       :value="$attrs.modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
       class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500"
